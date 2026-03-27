c1c6b168a409910c63242230c908777925ecaf4c
Alobesat582002 <hossynalobesat@gmail.com>
2026-03-27 15:44:31 +0300

    logo in title

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,7 @@
     <meta name="theme-color" content="#4f46e5">
     
     <title>Maintify - Home Facility Management</title>
+    <link rel="icon" type="image/png" href="/Maintify/assets/images/logo.png">
     
     <!-- BOOTSTRAP 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
